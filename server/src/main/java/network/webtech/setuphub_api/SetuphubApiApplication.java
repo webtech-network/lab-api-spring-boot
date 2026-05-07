5db356a30f94d3526d221194807530d6175a7b17
Artur Bomtempo Colen <arturbcolen@gmail.com>
2026-05-07 00:59:02 -0300

    feat: add initial project structure with Maven configuration and Spring Boot setup

---
+++ server/src/main/java/network/webtech/setuphub_api/SetuphubApiApplication.java
@@ -0,0 +1,13 @@
+package network.webtech.setuphub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetuphubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetuphubApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/network/webtech/setuphub_api/SetuphubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package network.webtech.setuphub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetuphubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
