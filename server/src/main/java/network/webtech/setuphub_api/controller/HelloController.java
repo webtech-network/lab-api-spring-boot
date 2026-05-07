f668c63e3b462f48ec47ff63a5138595e3d69c13
Artur Bomtempo Colen <arturbcolen@gmail.com>
2026-05-07 01:45:31 -0300

    feat: add HelloController with a greeting endpoint

---
+++ server/src/main/java/network/webtech/setuphub_api/controller/HelloController.java
@@ -0,0 +1,16 @@
+package network.webtech.setuphub_api.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        return Map.of(
+                "message", "Hello, SetuHub!");
+    }
+}
